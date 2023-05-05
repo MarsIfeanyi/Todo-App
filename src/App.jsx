501b0ea31f4d9bcda39422ba381d604d406154fb
MarsIfeanyi <marschalice@gmail.com>
2023-05-05 05:27:26 +0100

    Create and Render Home Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Home from "./components/home";
+
 const App = () => {
   return (
     <div className="mt-20 text-blue-500 text-center font-bold">
-      <h1>Todo App</h1>
+      <Home />
     </div>
   );
 };
