936cc7d13e8180a81d23f622b0cd3bfbae00e31c
MarsIfeanyi <marschalice@gmail.com>
2023-05-05 05:28:48 +0100

    Create Home component

---
+++ src/components/home/index.jsx
@@ -0,0 +1,16 @@
+import Footer from "./Footer";
+import MainNav from "./MainNav";
+
+const Home = () => {
+  return (
+    <>
+      <div>
+        <MainNav />
+
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default Home;
