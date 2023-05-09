5ae0c49b4fff56d15fe8b6e68a65a858d35eda11
MarsIfeanyi <marschalice@gmail.com>
2023-05-09 09:50:49 +0100

    Define and Add Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import Home from "./components/home";
+import { Routes, Route } from "react-router";
+import Footer from "./components/home/Footer";
+import HomePage from "./pages/home";
+import SignInPage from "./pages/signin";
+import SignUpPage from "./pages/signup";
 
 const App = () => {
   return (
-    <div className="mt-20 text-blue-500 text-center font-bold">
-      <Home />
+    <div className="mt-20 ">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+
+        <Route path="/sign-up" element={<SignUpPage />} />
+
+        <Route path="/sign-in" element={<SignInPage />} />
+      </Routes>
+
+      <Footer />
     </div>
   );
 };
