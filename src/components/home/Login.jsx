7d31e7f0a05dcc5dfe3f4f7e49b5705cba7cab2f
MarsIfeanyi <marschalice@gmail.com>
2023-05-09 09:40:32 +0100

    Create Login Component

--- src/components/home/Login.jsx
+++ src/components/home/Login.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
   return (
     <>
       <div>
-        <h1>Login</h1>
+        <Link to="/sign-in">
+          <button className="px-6 py-2 rounded-full bg-blue-700 text-white">
+            Sign In
+          </button>
+        </Link>
       </div>
     </>
   );
